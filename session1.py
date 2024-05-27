f6d0c2d9f7ab267dfdf1d234431cba3f18152bc7
230500 <230500@students.aih.nsw.edu.au>
2024-05-27 18:37:21 +1000

    Update session1.py

--- session1.py
+++ session1.py
@@ -1,23 +1,43 @@
-#This is my first file
-#Clalculate the area of circle
+# This is my first file
+# Clalculate the area of circle
 # A=πr^2
 # Area of Circle = pi*ra^2
-pi = 3.1415
-Dia = 14
-ra = Dia/2
+# pi = 3.1415
+# Dia = 14
+# ra = Dia/2
 
 
-input pi value
-input radius value
-calculate area of a circle by:
-pi times ra square
-display area of circle
+# input pi value
+# input radius value
+# calculate area of a circle by:
+# pi times ra square
+# display area of circle
 
 
-input kms
-input name of the user
-convert kms into miles by:
-  miles = 1.6 * kms
-display miles with name
+# input kms
+# input name of the user
+# convert kms into miles by:
+# miles = 1.6 * kms
+# display miles with name
 
+
+# write a program to input age of a user and let them know if they have the risk of diabetes or not?
+# use case is that a user with a age more than 65 is risky else not risky.
+age = 55
+if age >= 65
+  print("This is risky")
+else
+  print("Not at risk")
+
+# write a program to calculate the GST tax of two products, apple and pie
+# use case a apple has 10% GST and pies hats only 8% GST
+# Calculate total tax GST that needs to be deducted from the bill.
+# Apple gross sales is $75 and pie gross sales is $100.
+
+apple = 75
+pie = 100
+GST_apple = 0.1 * apple
+GST_pie = 0.08 * pie
+print(GST_apple)
+print(GST_pie)
 
