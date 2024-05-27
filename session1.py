2bfdf77aaddc8decf9b12f9d23f3e8b962a17add
230500 <230500@students.aih.nsw.edu.au>
2024-05-27 18:14:40 +1000

    Update session1.py

--- session1.py
+++ session1.py
@@ -1,1 +1,23 @@
+#This is my first file
+#Clalculate the area of circle
+# A=πr^2
+# Area of Circle = pi*ra^2
+pi = 3.1415
+Dia = 14
+ra = Dia/2
+
+
+input pi value
+input radius value
+calculate area of a circle by:
+pi times ra square
+display area of circle
+
+
+input kms
+input name of the user
+convert kms into miles by:
+  miles = 1.6 * kms
+display miles with name
+
 
