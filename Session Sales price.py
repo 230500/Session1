2a052a195f2cbbf8c1229adb663059626156e21e
230500 <230500@students.aih.nsw.edu.au>
2024-05-29 19:01:46 +1000

    Create Session Sales price.py

---
+++ Session Sales price.py
@@ -0,0 +1,15 @@
+comp = 2000
+mobile = 1600
+lapt = 1300
+earp = 200
+sw = 1100
+sv_comp = (0.13 * comp) + comp
+sv_mobile = (0.11 * mobile) + mobile
+sv_lapt = (0.12 * lapt) + lapt
+sv_earp = (0.07 * earp) + earp
+sv_sw = (0.09 * sw) + sw
+print("The sale value for Computer is: ", sv_comp)
+print("The sale value for Mobile is: ", sv_mobile)
+print("The sale value for Laptop is: ", sv_lapt)
+print("The sale value for Earphone is: ", sv_earp)
+print("The sale value for Smart Watch is: ", sv_sw)
