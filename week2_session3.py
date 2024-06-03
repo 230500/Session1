7d6ad879b49af8d93a53407a40b6c8cf8816e58a
230500 <230500@students.aih.nsw.edu.au>
2024-06-03 18:52:02 +1000

    Update week2_session3.py

--- week2_session3.py
+++ week2_session3.py
@@ -3,7 +3,20 @@
 
 age = int(input("Enter age: "))
 name = str(input("Enter name: "))
-if age >= 18:    #age is a dependant variable
+if age >= 18:                            #age is a dependant variable
     print("You are an adult", name)
 else:
     print("You are a minor", name)
+
+
+
+# While LOOP
+
+# Topic: The While Loop (Conditioned_Controlled)
+count = 1                             # Assigning count as 1.
+while count <=5 :                     # Using while condition loop and checking if its less than or equal to 5 (if true it print the frist output), in this case it's 1.
+    print(f"Counting.....{count}")    # Using "printf" to print {count} and after that
+    count += 1                        # Increment Count is executed and the count value (1) is increased by "+1" for the next loop.
+
+
+
